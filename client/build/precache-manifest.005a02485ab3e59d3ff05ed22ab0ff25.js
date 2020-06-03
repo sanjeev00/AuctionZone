3090ed590da232b14ff737f1121f4cab898fa11d
Sanjeev Krishna <34099368+sanjeev00@users.noreply.github.com>
2020-06-03 19:19:08 +0530

    gitignore change

---
+++ client/build/precache-manifest.005a02485ab3e59d3ff05ed22ab0ff25.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "805b15eca0a458eac946611d659a36e5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9217e88f050c229efc07",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "f746d69cfe54c3d05f5a",
+    "url": "/static/js/2.de3b39d0.chunk.js"
+  },
+  {
+    "revision": "dd207babaeb913476be46155cc504ef5",
+    "url": "/static/js/2.de3b39d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9217e88f050c229efc07",
+    "url": "/static/js/main.e6be4df8.chunk.js"
+  },
+  {
+    "revision": "0f7e17235ce2106ccee1",
+    "url": "/static/js/runtime-main.549be430.js"
+  }
+]);
